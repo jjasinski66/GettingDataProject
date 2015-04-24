f328ac6bb5ca3d4d2834fe8aae5637386448bae5
Jerry Jasinski <jjasinski66@users.noreply.github.com>
2015-04-24 14:46:26 -0500

    Cleanup

--- run_analysis.R
+++ run_analysis.R
@@ -18,11 +18,11 @@
 ##################################################################
 
 
-## read the TEST data 
+## read in the TEST data 
 X.test <- read.table("UCI HAR Dataset/test/X_test.txt", sep="", header=F)
 Y.test <- read.table("UCI HAR Dataset/test/y_test.txt", header=F)
 
-## read subject file
+## read in the subject file
 subject.test <- read.table("UCI HAR Dataset/test/subject_test.txt", header=F)
 
 # combine activity and subject to the X.test
@@ -30,11 +30,11 @@ X.test <- cbind(Y.test, X.test)
 X.test <- cbind(subject.test, X.test)
 
 
-## read the TRAIN data 
+## read in the TRAIN data 
 X.train <- read.table("UCI HAR Dataset/train/X_train.txt", sep="", header=F)
 Y.train <- read.table("UCI HAR Dataset/train/y_train.txt", header=F)
 
-# read subject file
+# read in the subject file
 subject.train <- read.table("UCI HAR Dataset/train/subject_train.txt", header=F)
 
 # add activity and subject to X.train
@@ -42,10 +42,10 @@ X.train <- cbind(Y.train, X.train)
 X.train <- cbind(subject.train, X.train)
 
 
-## merge TRAIN and TEST into one dataframe
+## merge TRAIN and TEST into one frame
 mergedset <- rbind(X.test, X.train)
 
-# remove tmp objects to free up memory
+# housecleaning
 rm(X.train, X.test, Y.train, Y.test, subject.test, subject.train)
 
 
@@ -57,25 +57,24 @@ rm(X.train, X.test, Y.train, Y.test, subject.test, subject.train)
 #read the list of 561 variables 
 features <- read.table("UCI HAR Dataset/features.txt", header=F)
 
-#name the variables in the dataframe includind the first two columns; 
-#later we make the names more descriptive. 
+#name the variables in the dataframe included the first two columns; 
 names(mergedset)[1] <- c("subject")
 names(mergedset)[2] <- c("activity")
 names(mergedset)[3:563] <- as.character(features$V2) 
 
-#extract all variables that measure either "mean" or "std"
+#get all variables that measure either "mean" or "std"
 ss <- grep("mean|std", names(mergedset))
 
-## subset frame to those columns which contain mean or standard deviation measurements
-##  preserve activity and subject columns as well
+## subset frame which contains mean or standard deviation measurements
+##  keep activity and subject columns as well
 mergedsetsub <- mergedset[, c(1,2,ss)]
 
-## take out the FFT variables since these contain the same information as the original 
-##   variables just in FFT form. Keep "t*" variables:
+## take out the FFT variables since these use the same information as the original 
+##  Keep "t*" variables:
 st <- grep("^[t]", names(mergedsetsub))
 mergedsetsub <- mergedsetsub[, c(1,2,st)]
 
-#clean up
+## housekeeping
 rm(ss, st)
 
 
@@ -83,13 +82,13 @@ rm(ss, st)
 # 3. Uses descriptive activity names to name the activities in the data set
 ###########################################################################
 
-# read the levels and lables of the activities
+# read the activity lables.
 l.activities <- read.table("UCI HAR Dataset/activity_labels.txt", header=F)
 
 #convert "activity" into descriptive factor. 
 mergedsetsub$activity <- factor(mergedsetsub$activity, levels=l.activities$V1, labels=l.activities$V2)
 
-#clean up
+## housekeeping
 rm(l.activities)
 
 
@@ -97,7 +96,7 @@ rm(l.activities)
 # 4. Fix the variable names
 ###########################
 
-#remove parenthesis "()" and "-" from the names.
+## remove "()" and "-" from the column names.
 
 names(mergedsetsub) <- gsub("[()]", "", names(mergedsetsub))
 names(mergedsetsub) <- gsub("[-]", "_", names(mergedsetsub))
